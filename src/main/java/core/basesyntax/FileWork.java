3653eda376875d45cf07265e8aba74c5c3050216
Tetiana Yashchuk <yashchuk.t.u@gmail.com>
2026-07-04 02:13:58 +0300

    feat: implement read from file logic

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        try {
+            String content = Files.readString(Path.of(fileName));
+            String[] words = content.toLowerCase().split("[\\s\\p{Punct}]+");
+
+            int count = 0;
+
+            for (String word : words) {
+                if (word.startsWith("w")) {
+                    count++;
+                }
+            }
+            String[] result = new String[count];
+            int index = 0;
+
+            for (String word : words) {
+                if (word.startsWith("w")) {
+                    result[index] = word;
+                    index++;
+                }
+            }
+
+            Arrays.sort(result);
+            return result;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+
     }
 }
